145d37bdd95cbc800e7003a980af4c9f0259e44c
Mark Kirby <mark@kirbyware.com>
2022-11-17 18:52:55 -0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `MarkSite`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
