55232d49a3b18ebce74ef1193bfdf029bfca5875
Mark Kirby <mark@kirbyware.com>
2022-11-29 16:49:56 -0500

    add styling

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,19 +1,23 @@
 import * as React from "react"
 import { Link } from "gatsby"
+import { container, heading, navLinks, navLinkItem, navLinkText } from './layout.module.css'
+console.log("container: ", container)
 
 const Layout = ({ pageTitle, pageHeading, children }) => { 
   return (
     <main>
-      <title>{pageTitle}</title>
-      <nav>
-        <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/about">About</Link></li>
-            <li><Link to="/blog">Blog</Link></li>
-        </ul>
-      </nav>
-      <h1>{pageHeading}</h1>
-      {children}
+        <title>{pageTitle}</title>
+        <div className={container}>
+            <nav>
+                <ul className={navLinks}>
+                    <li className={navLinkItem}><Link to="/" className={navLinkText}>Home</Link></li>
+                    <li className={navLinkItem}><Link to="/about" className={navLinkText}>About</Link></li>
+                    <li className={navLinkItem}><Link to="/blog" className={navLinkText}>Blog</Link></li>
+                </ul>
+            </nav>
+            <h1 className={heading}>{pageHeading}</h1>
+            {children}
+        </div>
     </main>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,8 +4,8 @@ import Layout from "../components/layout"
 
 const HomePage = () => { 
   return (
-    <Layout pageTitle="Home" pageHeading ="Welcome to my Gatsby site !">
-      <p>I am using the Gatsby tutorial</p>
+    <Layout pageTitle="Home" pageHeading ="Welcome to my Gatsby site!">
+      <p>I am builing this site to learn Gatsby using the tutorial <a href="https://www.gatsbyjs.com/docs/tutorial/part-2/" target="_blank">here</a></p>
     </Layout>
   )
 }
