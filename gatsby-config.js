77e0598d3d70f6d6b1759aaddc7b9651c8eee13c
Mark Kirby <mark@kirbyware.com>
2022-11-29 17:02:27 -0500

    complete tutorial 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,8 @@ module.exports = {
     title: `MarkSite`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,16 @@
 import * as React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/layout"
+import { StaticImage } from 'gatsby-plugin-image'
 
 const HomePage = () => { 
   return (
     <Layout pageTitle="Home" pageHeading ="Welcome to my Gatsby site!">
       <p>I am builing this site to learn Gatsby using the tutorial <a href="https://www.gatsbyjs.com/docs/tutorial/part-2/" target="_blank">here</a></p>
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="../images/Portsmouth01.jpg"
+      />
     </Layout>
   )
 }
